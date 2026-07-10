657e88992e1ac43c5a8c9d39f1e1d67fbd958107
SRIOUSS <changhui5796@gmail.com>
2026-07-10 17:50:45 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/insightonruleengine/InsightonRuleengineApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.insightonruleengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightonRuleengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightonRuleengineApplication.class, args);
+    }
+
+}
