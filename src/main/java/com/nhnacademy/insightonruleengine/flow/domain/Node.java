3507a79503d5894c3c2a8ce4705e1757ed987533
hiadela0802 <hiadela0802@gmail.com>
2026-08-04 15:51:47 +0900

    refact: import 최적화

--- src/main/java/com/nhnacademy/insightonruleengine/flow/domain/Node.java
+++ src/main/java/com/nhnacademy/insightonruleengine/flow/domain/Node.java
@@ -1,14 +1,7 @@
 package com.nhnacademy.insightonruleengine.flow.domain;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
