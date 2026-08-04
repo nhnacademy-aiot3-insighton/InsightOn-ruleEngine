b2486ef453ab0b4db9047aa738d321f767273d9a
hiadela0802 <hiadela0802@gmail.com>
2026-08-04 09:31:32 +0900

    feat: rabbitMQ로부터 엔진이 받아오기 위한 컨슈머

---
+++ src/main/java/com/nhnacademy/insightonruleengine/runner/messaging/SensorEventConsumer.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.insightonruleengine.runner.messaging;
+
+import com.nhnacademy.insightonruleengine.runner.FlowRunner;
+import com.nhnacademy.insightonruleengine.runner.dto.SensorEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+
+/**
+ *
+ */
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "", havingValue = "true")
+public class SensorEventConsumer {
+
+    private final FlowRunner flowRunner;
+
+    @RabbitListener(queues = "${}")
+    public void consume(SensorEvent event) {
+        flowRunner.run(event);
+    }
+}
