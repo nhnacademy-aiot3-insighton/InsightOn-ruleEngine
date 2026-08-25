651914b662b02d1563e0202c45e8743107e5dd3b
Wjdwodud2525 <nury252525353535@gmail.com>
2026-08-25 10:50:45 +0900

    Rename: CoreGroupClient url 주소 변경

--- src/main/java/com/nhnacademy/insightonruleengine/client/core/CoreGroupClient.java
+++ src/main/java/com/nhnacademy/insightonruleengine/client/core/CoreGroupClient.java
@@ -3,11 +3,12 @@ package com.nhnacademy.insightonruleengine.client.core;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "insighton-core", path = "/internal/groups", url = "${service-url.core}")
+@FeignClient(name = "insighton-core", path = "/internal/v1/groups", url = "${service-url.core}")
 public interface CoreGroupClient {
 
-    @GetMapping("/{group-id}/members/user/{user-id}")
+    @GetMapping("/{group-id}/members")
     GroupMemberResponse getGroupMemberByUserId(@PathVariable("group-id") Long groupId,
-                                               @PathVariable("user-id") Long userId);
+                                               @RequestParam("userId") Long userId);
 }
